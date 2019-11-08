78044311540939a61854decc77aac39c04c49a02
naveen0298 <naveenpolisetty0298@gmail.com>
2019-11-08 09:44:22 +0530

    added header,card,footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
